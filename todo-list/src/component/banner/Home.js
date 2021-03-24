0d37bd9ef33c141abf91b2ced396138349d56e36
Mystoryme <79404492+Mystoryme@users.noreply.github.com>
2021-03-24 21:17:57 +0700

    FlowerHoverWinkwink

--- todo-list/src/component/banner/Home.js
+++ todo-list/src/component/banner/Home.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "./Home.css";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import Flowerline from "../../img/flowerline.png";
 
 const Home = () => {
   return (
     <div className="banner">
-      <div className="bannerLeft">aaa</div>
-      <div className="bannerRight">bbb</div>
+      <div className="boxup">
+        <h1
+          style={{
+            margin: "70px",
+          }}
+        >
+          My.Plan
+        </h1>
+        <img src={Flowerline}></img>
+      </div>
+      <div className="getstart">
+        <Link
+          to="/login"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <Button
+            variant="contained"
+            style={{
+              background: "white",
+              transform: "translateY(-115px)",
+            }}
+          >
+            Get Started
+          </Button>
+        </Link>
+      </div>
+      {/* <div className="bannerLeft">aaa</div>
+      <div className="bannerRight">bbb</div> */}
     </div>
   );
 };
