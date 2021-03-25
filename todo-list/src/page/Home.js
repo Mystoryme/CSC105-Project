9fa0bee6f92fc637500df07b710b8762e5c7ce82
Mystoryme <thanyapatismine@gmail.com>
2021-03-25 22:03:10 +0700

    add cloud

--- todo-list/src/page/Home.js
+++ todo-list/src/page/Home.js
@@ -3,13 +3,17 @@ import "../style/Home.css";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Flowerline from "../img/flowerline.png";
+import Cloud from "../img/cloud.png";
 
 const Home = () => {
   return (
     <div className="banner">
       <div className="boxup">
+        <img className="cloud" src={Cloud}></img>
+        <img className="cloud2" src={Cloud}></img>
+
         <h1>MyPlan</h1>
-        <img src={Flowerline}></img>
+        <img className="flower" src={Flowerline}></img>
       </div>
       <div className="getstart">
         <Link
